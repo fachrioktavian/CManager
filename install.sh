17f18d55a6a6c17808e012687588d213f4e991f3
fachrioktavian <fachri0510@gmail.com>
2016-12-05 23:01:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ then
 	echo "Please specify active user as parameter"
 else
 	user=$1
-	sudo pip install colorama terminaltables pbkdf2 netifaces
+	sudo pip install colorama terminaltables pbkdf2 netifaces python-wifi
 	sudo echo "$user ALL=(ALL) NOPASSWD: $(which iwlist)" >> /etc/sudoers
 	sudo echo "$user ALL=(ALL) NOPASSWD: $(which wpa_supplicant)" >> /etc/sudoers
 	sudo echo "$user ALL=(ALL) NOPASSWD: $(which dhclient)" >> /etc/sudoers
