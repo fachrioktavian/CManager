bc2b669da484199e279303621378b4eb202bd0e1
fachrioktavian <fachri0510@gmail.com>
2016-12-07 11:31:18 +0700

    prevent problem with sudoers file

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ then
 else
 	user=$1
 	sudo pip install colorama terminaltables pbkdf2 netifaces python-wifi
-	sudo echo "$user ALL=(ALL) NOPASSWD: $(which iwlist)" >> /etc/sudoers
-	sudo echo "$user ALL=(ALL) NOPASSWD: $(which wpa_supplicant)" >> /etc/sudoers
-	sudo echo "$user ALL=(ALL) NOPASSWD: $(which dhclient)" >> /etc/sudoers
-	sudo echo "$user ALL=(ALL) NOPASSWD: $(which pkill)" >> /etc/sudoers
+	sudo echo "ALL ALL=(ALL) NOPASSWD: $(which iwlist)" >> /etc/sudoers
+	sudo echo "ALL ALL=(ALL) NOPASSWD: $(which wpa_supplicant)" >> /etc/sudoers
+	sudo echo "ALL ALL=(ALL) NOPASSWD: $(which dhclient)" >> /etc/sudoers
+	sudo echo "ALL ALL=(ALL) NOPASSWD: $(which pkill)" >> /etc/sudoers
 	sudo mkdir /usr/share/dcm
 	sudo cp -R * /usr/share/dcm/
 	sudo ln -s /usr/share/dcm/dcm.py /bin/dcm
