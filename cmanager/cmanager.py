441f76806ecb722ee32324e7bff0e09a2b923ced
agusmakmun <summon.agus@gmail.com>
2016-12-14 03:49:17 +0700

    fixed initial configuration

--- cmanager/cmanager.py
+++ cmanager/cmanager.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Fachrizal Oktavian
-# This file is part of CManager.
+# This file is part of DracOS Connection Manager.
 #
-# CManager is free software: you can redistribute it and/or modify
+# DracOS Connection Manager is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# CManager is distributed in the hope that it will be useful,
+# DracOS Connection Manager is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with CManager.  If not, see <http://www.gnu.org/licenses/>.
+# along with DracOS Connection Manager.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
 from core.cm_app import Cm_app
 
 
 def main():
+    if os.getuid() != 0:
+        print ("[-] Please Access as root..!")
+        sys.exit()
+
+    if len(sys.argv) > 1:
+        configure = sys.argv[1]
+        if configure.lower() == 'configure':
+            os.system('./configure.sh')
+            sys.exit()
+
     cm_app_handler = Cm_app()
     cm_app_handler.standby()
 
---
+++ cmanager/configure.sh
@@ -0,0 +1,48 @@
+#/bin/sh
+
+iwlistbin=$(which iwlist)
+wpabin=$(which wpa_supplicant)
+dhclientbin=$(which dhclient)
+pkillbin=$(which pkill)
+
+if [ -z "$iwlistbin" ] || [ -z "$wpabin" ] || [ -z "$dhclientbin" ] || [ -z "$pkillbin" ]
+then
+	echo "One of these program are not founded in your system:"
+	echo "	- iwlist"
+	echo "	- wpa_supplicant"
+	echo "	- dhclient"
+	echo "	- pkill"
+else
+	sudoersiwlist=$(sudo cat /etc/sudoers | grep "ALL ALL=(ALL) NOPASSWD: $iwlistbin")
+	sudoerswpa=$(sudo cat /etc/sudoers | grep "ALL ALL=(ALL) NOPASSWD: $wpabin")
+	sudoersdhclient=$(sudo cat /etc/sudoers | grep "ALL ALL=(ALL) NOPASSWD: $dhclientbin")
+	sudoerspkill=$(sudo cat /etc/sudoers | grep "ALL ALL=(ALL) NOPASSWD: $pkillbin")
+
+	if [ $sudoersiwlist ]
+	then
+		echo "[INSTALL] iwlist has been in sudoers whitelist"
+	else
+		sudo echo "ALL ALL=(ALL) NOPASSWD: $iwlistbin" >> /etc/sudoers
+	fi
+
+	if [ $sudoerswpa ]
+	then
+		echo "[INSTALL] wpa_supplicant has been in sudoers whitelist"
+	else
+		sudo echo "ALL ALL=(ALL) NOPASSWD: $wpabin" >> /etc/sudoers
+	fi
+
+	if [ $sudoersdhclient ]
+	then
+		echo "[INSTALL] dhclient has been in sudoers whitelist"
+	else
+		sudo echo "ALL ALL=(ALL) NOPASSWD: $dhclientbin" >> /etc/sudoers
+	fi
+
+	if [ $sudoerspkill ]
+	then
+		echo "[INSTALL] pkill has been in sudoers whitelist"
+	else
+		sudo echo "ALL ALL=(ALL) NOPASSWD: $pkillbin" >> /etc/sudoers
+	fi
+fi
