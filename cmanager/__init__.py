ff49f7a2f89342b6b9ad62134d537e4e997ad399
fachrioktavian <fachri0510@gmail.com>
2016-12-15 11:59:58 +0700

    Update __init__.py

--- cmanager/__init__.py
+++ cmanager/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.4'
+__version__ = '2.5'
 __author__ = 'Fachrizal Oktavian'
 __author_email__ = 'fachri0510@gmail.com'
-__release_date__ = 'December 14th, 2016'
+__release_date__ = 'December 15th, 2016'
